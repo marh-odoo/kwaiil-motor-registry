42bcfb24200be063433120b4b12f8d9a1fb02264
Mauricio Rubio Haro <marh@odoo.com>
2023-07-18 10:56:19 -0600

    First Commit

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Motorcycle Registry',
+    'summary' : 'Manage Registration of Motorcycles',
+    'description' : """Motorcycle Registry
+====================
+This Module is used to keep track of the Motorcycle Registration and Ownership of each motorcycled of the brand.
+    """,
+    'license' : 'OPL-1',
+    'author' : 'Mauricio Rubio',
+    'website' : 'www.odoo.com',
+    'category' : 'Custom Modules / Technical Training',
+    'depends' : ['base'],
+    'data': [],
+    'demo': [],
+    'application' : True,
+}   
